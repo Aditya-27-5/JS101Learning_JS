0ebff5073608bf206c3eab829f55fda91ca586fb
Aditya Bhatnagar <adityab40527@gmail.com>
2022-09-22 20:04:27 +0530

    Mathematical Operators

---
+++ Mathematical-Operators-Assignment/problem2.js
@@ -0,0 +1,15 @@
+let one = 1
+let two = 2
+let three = 3
+let four = 4
+let five = 5
+
+let  = one * 2;  
+let = two * 2
+let = three * 2
+let  = four * 2
+ let =  five * 2; 
+
+let sum = one * 2  + two * 2 + three * 2 +
+ four * 2 + five * 2
+console.log (sum)
---
+++ Mathematical-Operators-Assignment/problem3.js
@@ -0,0 +1,20 @@
+let one = 1
+let two = 2
+let three = 3
+let four = 4
+let five = 5
+let six = 6
+//The value stored in the variabletwo, five, sixhas been doubled, such that if initially the value was 3, it has become 6
+let = two * 2
+let = five * 2
+let = six * 2
+//The value stored in the variablethree, fourhas been increased three times, such that if initially it was 3, it has become 9
+let = three * 3
+let  = four * 3
+//The value stored in the variable one has not been changed.
+
+
+
+let sum = one + two * 2 + five * 2 + six * 2 + three * 3 + four * 3
+
+console.log (sum)
